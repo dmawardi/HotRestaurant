9dc9f690e30b282839bfbf2da8aa01e693ca5bc6
Dannar Mawardi <dmawardi@gmail.com>
2019-09-07 12:52:07 +1000

    added data, reservation view populating

--- app/data/reservationList.js
+++ app/data/reservationList.js
@@ -4,7 +4,25 @@ var reservationList = [
         "email": "James.rudd@james.rudd",
         "Phone": "1234-5678",
         "uID": "abc123"
+    },
+    {
+        "name": "James Rudd",
+        "email": "James.rudd@james.rudd",
+        "Phone": "1234-5678",
+        "uID": "abc123"
+    },
+    {
+        "name": "James Rudd",
+        "email": "James.rudd@james.rudd",
+        "Phone": "1234-5678",
+        "uID": "abc123"
+    },{
+        "name": "James Rudd",
+        "email": "James.rudd@james.rudd",
+        "Phone": "1234-5678",
+        "uID": "abc123"
     }
+    
 ]
 
 module.exports = reservationList;
